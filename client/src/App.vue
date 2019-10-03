3c2274a4a887b6c92679e296d1c9dcb88522515f
7SJ7 <stankojankov99@gmail.com>
2019-10-04 01:52:15 +0200

    small design changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/products">Products</router-link>
+      <router-link to="/products">Products List</router-link> |
+      <router-link to="/product">Product</router-link>
     </div>
     <router-view/>
   </div>
@@ -10,11 +11,13 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  font-family: 'Cursive';
+  -webkit-font-smoothing: subpixel-antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: beige;
+  padding: 50px;
 }
 #nav {
   padding: 30px;
@@ -26,6 +29,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #5D6D7E;
 }
 </style>
--- client/src/views/Product.vue
+++ client/src/views/Product.vue
@@ -48,13 +48,9 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 a {
   color: #42b983;
-}
-.createButton {
-  margin-bottom: 1em;
 }
 .products {
   margin-left: 5%;
