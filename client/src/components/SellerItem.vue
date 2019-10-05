18399cfe73b32b777da439be3f80464413c06ac8
7SJ7 <stankojankov99@gmail.com>
2019-10-05 18:30:01 +0200

    seller-item

---
+++ client/src/components/SellerItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-list-group-item>
+    Name:{{ seller.name }}
+    <br><br>
+  </b-list-group-item>
+</template>
+
+<script>
+
+export default {
+  name: 'seller-item',
+  props: ['seller']
+}
+</script>
+
+<style scoped>
+img {
+  width: 33px;
+  margin-right: 10px;
+}
+</style>
